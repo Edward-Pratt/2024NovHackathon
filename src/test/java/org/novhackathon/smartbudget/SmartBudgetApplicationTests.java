354b277d2475ef854951a0655604ec675579e581
Edward Pratt <41444303+Edward-Pratt@users.noreply.github.com>
2024-11-27 11:28:11 +0000

    First

---
+++ src/test/java/org/novhackathon/smartbudget/SmartBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package org.novhackathon.smartbudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBudgetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
