f6e025de7b679ce676dce551bbd9a23c2116c8e9
Edward Pratt <41444303+Edward-Pratt@users.noreply.github.com>
2024-11-27 15:00:28 +0000

    Total Amount

--- src/main/java/org/novhackathon/smartbudget/controller/TransactionController.java
+++ src/main/java/org/novhackathon/smartbudget/controller/TransactionController.java
@@ -1,10 +1,13 @@
 package org.novhackathon.smartbudget.controller;
 
 import model.Transaction;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -24,4 +27,16 @@ public class TransactionController {
     public List<Transaction> getTransactions() {
         return transactions;
     }
+    @GetMapping("/transactionsTotal")
+    public double totalTransactions() {
+        double total = 0;
+        for (Transaction transaction : transactions) {
+            total += transaction.getAmount();
+        }
+        return total;
+
+    }
+
+
+
 }
